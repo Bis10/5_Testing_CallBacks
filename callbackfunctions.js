81aca2bbba4653ea8a2b7ae91fe5bf5372d9dc27
Suyel Bishnu <suyel2007@gmail.com>
2024-09-23 10:09:57 +0300

    Add new delay function to print

--- callbackfunctions.js
+++ callbackfunctions.js
@@ -5,7 +5,7 @@ function printNiceText(text, cb){
 cb (text);
 }
 
-printNiceText('some text', console.log);
+// printNiceText('some text', console.log);
 
 // printNiceText('more text',(text)=>{
 //     console.log('##########');
@@ -19,4 +19,13 @@ function print(text){
     console.log("*********");
 }
 
-printNiceText('some additional text', print);
+// printNiceText('some additional text', print);
+
+function printWhenReady(text, cb){
+    setTimeout(() => cb(text), Math.random()*3000);
+}
+
+printWhenReady('XYZ', console.log);
+printWhenReady('ABC', print);
+
+console.log('This is the end');
