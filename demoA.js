b96c4658432d6aea0fdc18e9f999f2cb150358e8
Suyel Bishnu <suyel2007@gmail.com>
2024-09-23 10:45:02 +0300

    Create a demo test to search the car

---
+++ demoA.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const search = require('./carStorageNoFileop');
+
+console.log(search());
+console.log('##### ONE #####');
+console.log(search('model', 'Nova'));
+console.log('##### TWO #####');
+console.log(search('licence','ABC-1'));
+console.log('##### THREE #####');
+console.log(search('model', 'Bored T-model'));
+console.log('##### FOUR #####');
