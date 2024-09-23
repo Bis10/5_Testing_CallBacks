faa6e7b1dfc844f4206c13e6197db0779832fde1
Suyel Bishnu <suyel2007@gmail.com>
2024-09-23 12:37:50 +0300

    Create new version of car storage callback

--- demoB.js
+++ demoB.js
@@ -8,5 +8,30 @@ search(console.log, "model", "Nova");
 console.log("##### TWO #####");
 search(console.log, "licence", "ABC-1");
 console.log("##### THREE #####");
-search(console.log,"model", "Bored T-model");
+search(console.log, "model", "Bored T-model");
 console.log("##### FOUR #####");
+
+search((data) => console.log("found:", data), "licence", "ABC-1");
+
+search(
+  (data) => {
+    if (data.length > 0) {
+      const car = data[0];
+      console.log(`model: ${car.model},${car.licence}`);
+    }
+  },
+  "licence",
+  "ABC-1"
+);
+
+function printData(data) {
+  console.log("########");
+  for (const car of data) {
+    console.log(`model: ${car.model},${car.licence}`);
+  }
+  console.log("########");
+}
+
+search(printData);
+
+search(printData, "model", "Bored T-model");
